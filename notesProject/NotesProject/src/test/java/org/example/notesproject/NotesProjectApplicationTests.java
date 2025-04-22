8eb444b9980dc122f5ffe69a6fba61b8f391698f
Stefcho1227 <stefan.ivanov1227@gmail.com>
2025-04-22 17:05:11 +0300

    created the spring project

---
+++ notesProject/NotesProject/src/test/java/org/example/notesproject/NotesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.notesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
